06c318b1cdf9b3e15985b01e92302dd0a5aa59b7
Мария <masha2355@gmail.com>
2023-07-16 23:59:04 +1000

    Three

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -53,19 +53,19 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndOverLimit() {
-            BonusService service = new BonusService();
+    @Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
 
-            // подготавливаем данные:
-            long amount = 1_000_000;
-            boolean registered = false;
-            long expected = 500;
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            Assertions.assertEquals(expected, actual);
-        }
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 }
